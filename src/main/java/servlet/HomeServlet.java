25044b125ab6d8be82d03fb65b7b7808f1f87fb1
ソン <anhhangxom997@gmail.com>
2025-06-20 09:44:21 +0900

    新規作成と修正したファイルは：

--- src/main/java/servlet/HomeServlet.java
+++ src/main/java/servlet/HomeServlet.java
@@ -1,35 +1,37 @@
 package servlet;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
-import java.io.IOException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession session = req.getSession(false);
 
      
         if (session == null || session.getAttribute("staffId") == null) {
-            resp.sendRedirect(req.getContextPath() + "/WEB-INF/views/login.jsp");
+            res.sendRedirect(req.getContextPath() + "/WEB-INF/views/login.jsp");
             return;
         }
 
-        
         String position = (String) session.getAttribute("position");
-
- 
-        switch (position) {
-	        case "manager":
-	            req.getRequestDispatcher("/WEB-INF/views/managerMain.jsp").forward(req, resp);
-	            break;
-	        case "staff":
-	        default:
-	            req.getRequestDispatcher("/WEB-INF/views/staffMenu.jsp").forward(req, resp);
-	            break;
-	    }
+        String department = (String) session.getAttribute("department");
+        
+        if(("一般社員".equals(position) || "主任".equals(position)) && "営業部".equals(department)) {
+        	req.getRequestDispatcher("/WEB-INF/views/staffMenu.jsp").forward(req, res);
+        }else if("一般社員".equals(position) && "管理部".equals(department)) {
+        	req.getRequestDispatcher("/WEB-INF/views/managerMain.jsp").forward(req, res);
+        }else if("部長".equals(position) && "営業部".equals(department)) {
+        	req.getRequestDispatcher("/WEB-INF/views/buchougamen.jsp").forward(req, res);
+        }
+        
     }
 }
